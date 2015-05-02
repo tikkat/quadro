1703f65e2944d37369be200df5e78deb77e848a9
Tobias Tikka <tobias.tikka@gmail.com>
2015-05-02 23:49:32 +0200

    drag and drop working

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,35 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("task", ev.target.id);
 }
 
 function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+  ev.preventDefault();
+  var angularScope = angular.element($("body")).scope();
+  console.log(angularScope);
+  var data = ev.dataTransfer.getData("task");
+
+  if(ev.target.className.indexOf('task-element') > -1) {
+    console.log(ev.target.dataset.quadrant);
+  } else {
+    switch (ev.target.id) {
+      case 'first':
+        console.log("1");
+        break;
+      case 'second':
+        console.log("2");
+        break;
+      case 'third':
+        console.log("3");
+        break;
+      case 'fourth':
+        console.log("4");
+        break;
+      default:
+        return;
+    }
+  }
+
+  ev.target.appendChild(document.getElementById(data));
 }
