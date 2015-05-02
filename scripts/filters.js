2cc31aa1ec6dfe80b8f5856ec8003f15352123b2
Tobias Tikka <tobias.tikka@gmail.com>
2015-05-02 10:35:20 +0200

    task lists are now getting their info from the main scope, removed undefined error in javascript console

--- scripts/filters.js
+++ scripts/filters.js
@@ -2,7 +2,7 @@ var fourQuadrantsFilters = angular.module('fourQuadrantsFilters', []);
 
 fourQuadrantsFilters.filter('reverse', function() {
   return function(items) {
-
+    if(items !== undefined)
       return items.slice().reverse();
   };
 });
